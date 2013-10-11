bf4fda065ac0042e0a38bf7ddd5a1c33e38a97d8
anabellaGuerrero <guerreroanabella@gmail.com>
2013-10-11 16:27:13 -0500

    codigo practica incompleto
    falta cambiar a string dtos

---
+++ src/Logica/Prueba.java
@@ -0,0 +1,25 @@
+package Logica;
+
+public class Prueba {
+	public static void main(String[] args) {
+		// ATRIBUTOS
+		Prueba p = new Prueba();
+	    p.imprimir();
+	    String CualquierCosa = "holis";
+	   
+	    
+	}
+	
+	int numero [  ]= {1,2,3,4,5,6,7,8,9,10};
+	
+	public void imprimir(){
+		for ( int i=0; i<10;i++){
+			System.out.println(numero[i]);
+			System.out.println("miau");
+			System.out.println(numero.toString());
+		}
+
+	}
+
+
+	}
